ef4cdc4925fcce780e274ac2b581151754c39d58
Inashi100 <Inashi100@users.noreply.github.com>
2023-07-19 12:14:55 +0100

    game created and played in browsers console

--- rps.js
+++ rps.js
@@ -1,1 +1,31 @@
-prompt("Testing the link")
+function getComputerChoice(){
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+    }
+    let playerChoice = prompt('hey! enter choice')
+    
+    function game(){
+        playerChoice;
+        const computerChoice = getComputerChoice();
+        console.log(playerChoice)
+        console.log(computerChoice)
+        switch(playerChoice + computerChoice){
+            case 'paperrock':
+            case 'rockscissors':
+            case 'scissorspaper':
+                console.log('player wins')
+                break;
+            case 'rockpaper':
+            case 'scissorsrock':
+            case 'paperscissors':
+                console.log('computer wins')
+                break;
+            case 'rockrock':
+            case 'paperpaper':
+            case 'scissorsscissors':
+                console.log('Its a draw');
+                break;
+        }
+    }
+    game()
