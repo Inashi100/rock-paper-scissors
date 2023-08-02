3d8cc038af773d323bb870ba37b8c3775efb21a8
Inashi100 <Inashi100@users.noreply.github.com>
2023-08-02 05:31:45 +0100

    playround to be revisited.

--- rps.js
+++ rps.js
@@ -2,9 +2,11 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
+const gameRounds = document.querySelector('playRound')
 let userChoice
 let ComputerChoice
 let result
+let playRound
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
    userChoice = e.target.id
